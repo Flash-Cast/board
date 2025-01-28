01c1cbd5e7937e9acd754f27d6a837bc9d0bae1c
Ryuta Tsukui <rivermouthlake@gmail.com>
2025-01-28 20:28:31 +0900

    Delete my_project/urls.py

--- my_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include   # Add
-
-
-urlpatterns = [
-    path('board_app/', include('board_app.urls')),   # Add
-    path('admin/', admin.site.urls),
-]
